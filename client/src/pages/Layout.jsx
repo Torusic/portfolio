d8f87e858b3210a1713bdfa3613b6ae23602c761
Torusic <smuchuku7@gmail.com>
2026-02-04 09:12:04 +0300

    icons

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -3,6 +3,10 @@ import logo from "../assets/logo.png";
 import Divider from '../Components/Divider';
 import profile from '../assets/simo.jpeg';
 import { Link, Outlet } from 'react-router-dom';
+import { IoHomeSharp } from "react-icons/io5";
+import { MdWork } from "react-icons/md";
+import { LuMessageCircleMore } from "react-icons/lu";
+import { CgProfile } from "react-icons/cg"
 
 function Layout() {
   return (
@@ -10,11 +14,11 @@ function Layout() {
       {/* Header stays here */}
       <header className='flex justify-between sticky  top-0 p-2 lg:p-3 h-15 bg-blue-100 items-center'>
         <img src={logo} width={120} height={70} alt="Logo"/>
-        <div className='flex items-center text-sm cursor-pointer font-semibold text-blue-500 justify-between gap-2 lg:gap-4'>
-          <p><Link to="/">Home</Link></p>
-          <p><Link to="/about">About</Link></p>
-          <p><Link to="/project">Projects</Link></p>
-          <p><Link to="/contact">Contacts</Link></p>
+        <div className='flex items-center text-sm cursor-pointer font-semibold text-blue-500 justify-between gap-5 lg:gap-4'>
+          <p><Link to="/"><IoHomeSharp size={20} /></Link></p>
+          <p><Link to="/about"><CgProfile size={20} /></Link></p>
+          <p><Link to="/project"><MdWork size={20} /></Link></p>
+          <p><Link to="/contact"><LuMessageCircleMore size={20} /></Link></p>
         </div>
       </header>
 
